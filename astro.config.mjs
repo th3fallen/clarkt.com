e0717ecc37483ff8a6752c551ae444a199575146
Clark Tomlinson <fallen013@gmail.com>
2026-08-28 15:32:45 -0400

    skaffold workderd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://clarkt.com',
   integrations: [sitemap()],
+
   build: {
     inlineStylesheets: 'always',
   },
+
+  adapter: cloudflare(),
 });
